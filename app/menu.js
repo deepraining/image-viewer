3bf5182ce9e0673c6a08e0bc8059bf33adb80fee
senntyou <jiangjinbelief@163.com>
2018-08-20 17:13:42 +0800

    update

--- app/menu.js
+++ app/menu.js
@@ -122,43 +122,41 @@ export default class MenuBuilder {
       {
         label: 'View',
         submenu:
-          process.env.NODE_ENV === 'development'
-            ? [
-                {
-                  label: 'Reload',
-                  accelerator: 'Ctrl+R',
-                  click: () => {
-                    this.mainWindow.webContents.reload();
-                  }
-                },
-                {
-                  label: 'Toggle Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  }
-                },
-                {
-                  label: 'Toggle Developer Tools',
-                  accelerator: 'Alt+Ctrl+I',
-                  click: () => {
-                    this.mainWindow.toggleDevTools();
-                  }
-                }
-              ]
-            : [
-                {
-                  label: 'Toggle Full Screen',
-                  accelerator: 'F11',
-                  click: () => {
-                    this.mainWindow.setFullScreen(
-                      !this.mainWindow.isFullScreen()
-                    );
-                  }
-                }
-              ]
+          // process.env.NODE_ENV === 'development' ?
+          [
+            {
+              label: 'Reload',
+              accelerator: 'Ctrl+R',
+              click: () => {
+                this.mainWindow.webContents.reload();
+              }
+            },
+            {
+              label: 'Toggle Full Screen',
+              accelerator: 'F11',
+              click: () => {
+                this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+              }
+            },
+            {
+              label: 'Toggle Developer Tools',
+              accelerator: 'Alt+Ctrl+I',
+              click: () => {
+                this.mainWindow.toggleDevTools();
+              }
+            }
+          ]
+        // : [
+        //     {
+        //       label: 'Toggle Full Screen',
+        //       accelerator: 'F11',
+        //       click: () => {
+        //         this.mainWindow.setFullScreen(
+        //           !this.mainWindow.isFullScreen()
+        //         );
+        //       }
+        //     }
+        //   ]
       },
       {
         label: 'Options',
